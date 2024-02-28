85eb0f9e3d7848a185b2c53c9a0861b6d1cca849
joedoe <d_joe@gmx.net>
2024-02-28 23:28:59 +0100

    caching added

--- v2/js/app.js
+++ v2/js/app.js
@@ -13,7 +13,7 @@ let btn, listenerDisabled = true;
 let clusters, players, human, playerIndex = 0;
 
 function main() {
-  registerServiceWorker();
+  registerServiceWorker(true);
   setSizes(window.innerHeight);
   [board, centerNode] = createBoard();
   loadImages().then(() => {
@@ -152,15 +152,15 @@ function toggleHidden(ids) {
   for (let id of ids) document.getElementById(id).classList.toggle("hidden");
 }
 
-function registerServiceWorker() {
+function registerServiceWorker(prod) {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', async () => {
       try {
-        if (import.meta.env?.DEV) {
+        if (!prod) {
           await navigator.serviceWorker.register('/service-worker.js', {
             type: 'module',
           });
-        } else await navigator.serviceWorker.register('/service-worker.js');
+        } else await navigator.serviceWorker.register('/dicewars/service-worker.js');
       } catch (err) {
         console.log('😥 Service worker registration failed: ', err);
       }
